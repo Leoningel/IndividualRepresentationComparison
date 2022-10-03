0b186697eb5ec0ff7a8f13c4b9a1f17a3b0f02bc
Leon Ingelse <leoningelse@gmail.com>
2022-10-03 14:45:36 +0200

    Create parallel_comparison.sh

---
+++ parallel_comparison.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=leon_ingelse_thesis  # Job name
+#SBATCH --nodes=1                       # Run all processes on a single node	
+#SBATCH --ntasks=1                      # Run a single task		
+#SBATCH --cpus-per-task=1               # Number of CPU cores per task
+#SBATCH --mem=1gb                       # Job memory request
+#SBATCH --time=48:00:00                 # Time limit hrs:min:sec
+#SBATCH --output=logs/parallel_%j.log   # Standard output and error log
+
+#SBATCH --array=0-1169                   # iterate values between 0 and 59, inclusive
+
+bash setup.sh
+python src/single_example.py -s $(expr $SLURM_ARRAY_TASK_ID % 30) -e $(expr $SLURM_ARRAY_TASK_ID / 90) -r $(expr $(expr $SLURM_ARRAY_TASK_ID / 30) % 3) $*
