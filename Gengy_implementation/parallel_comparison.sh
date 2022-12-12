78a26022b4547929e77d36b529660ed2b9db440f
Leon Ingelse <leoningelse@gmail.com>
2022-12-12 11:48:36 +0000

    Update parallel_comparison.sh

--- Gengy_implementation/parallel_comparison.sh
+++ Gengy_implementation/parallel_comparison.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=lingelse_rep_comp    # Job name
+#SBATCH --job-name=rep_comp             # Job name
 #SBATCH --nodes=1                       # Run all processes on a single node	
 #SBATCH --ntasks=1                      # Run a single task		
 #SBATCH --cpus-per-task=1               # Number of CPU cores per task
-#SBATCH --mem=1gb                       # Job memory request
+#SBATCH --mem=6gb                       # Job memory request
 #SBATCH --time=48:00:00                 # Time limit hrs:min:sec
 #SBATCH --output=logs/parallel_%j.log   # Standard output and error log
 
