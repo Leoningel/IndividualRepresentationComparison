6569c4a3f6222dbf53ae53fccd9eb98a03872f14
Leon Ingelse <leoningelse@gmail.com>
2022-12-12 14:12:10 +0000

    Nothing much

--- Gengy_implementation/parallel_comparison.sh
+++ Gengy_implementation/parallel_comparison.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=rep_comp             # Job name
+#SBATCH --job-name=rep_comp_cnn         # Job name
 #SBATCH --nodes=1                       # Run all processes on a single node	
 #SBATCH --ntasks=1                      # Run a single task		
 #SBATCH --cpus-per-task=1               # Number of CPU cores per task
-#SBATCH --mem=6gb                       # Job memory request
+#SBATCH --mem=10gb                      # Job memory request
 #SBATCH --time=48:00:00                 # Time limit hrs:min:sec
 #SBATCH --output=logs/parallel_%j.log   # Standard output and error log
 
-#SBATCH --array=0-89                   # iterate values between 0 and 59, inclusive
+#SBATCH --array=0-89%5                  # iterate values between 0 and 59, inclusive
 
 # sbatch parallel_comparison.sh treebased
 cd ..
