f23b61d4ff34d2548cf028c021d1e2dfb56fea81
Leon Ingelse <leoningelse@gmail.com>
2022-12-14 10:16:45 +0000

    HPO ready!

--- Gengy_implementation/examples/HPO.py
+++ Gengy_implementation/examples/HPO.py
@@ -13,6 +13,14 @@ from geneticengine.metahandlers.vars import VarRange
 
 from utils.wrapper import run_experiments
 import utils.global_vars as gv
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
 
 
 @abstract
@@ -23,6 +31,7 @@ class Start():
 class Model():
     pass
 
+@abstract
 class LinearM(Model):
     pass
 
@@ -34,6 +43,7 @@ class NNet(Model):
 class Kernel():
     pass
 
+@abstract
 class Linear(Kernel):
     pass
 
@@ -43,12 +53,12 @@ class Polynomial(Kernel):
 
 @dataclass
 class Radial(Kernel):
-    gamma: Annotated[float, FloatList(0.1, 0.2, 0.5, 1)]
+    gamma: Annotated[float, FloatList([0.1, 0.2, 0.5, 1])]
 
 @dataclass
 class SVM(Model):
     kernel: Kernel
-    cost: Annotated[float, FloatList(0.1, 1, 10, 100, 1000)]
+    cost: Annotated[float, FloatList([0.1, 1, 10, 100, 1000])]
 
 @abstract
 class Features():
@@ -56,41 +66,76 @@ class Features():
 
 @dataclass
 class FeatureList(Features):
-    time: bool
-    chick: bool
-    diet: bool
+    time: Annotated[int, IntList([0, 1])]
+    chick: Annotated[int, IntList([0, 1])]
+    diet: Annotated[int, IntList([0, 1])]
 
 @dataclass
 class Solution(Start):
     model: Model
-    features: Features
+    features: FeatureList
 
 grammar = extract_grammar(
                 considered_subtypes=[Start, Solution, Model, Features, FeatureList, LinearM, NNet, Kernel, Linear, Polynomial, Radial, SVM ],
                 starting_symbol=Start,
                 )
 
-def evaluate(elem: Solution, data):
-    return None
+data = pd.read_csv('examples/data/ChickWeight.csv')
+X = data[['Time', 'Chick', 'Diet']]
+y = data['weight']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+data_train = (X_train, y_train)
 
+def evaluate(elem: Solution, data_test):
+    features = list()
+    if bool(elem.features.time):
+        features.append('Time')
+    if bool(elem.features.chick):
+        features.append('Chick')
+    if bool(elem.features.diet):
+        features.append('Diet')
+    if not features:
+        return None
+    else:
+        X_train = data_train[0][features].values
+        y_train = data_train[1].values
+        X_test = data_test[0][features].values
+        y_test = data_test[1].values
+    if isinstance(elem.model, LinearM):
+        model = LinearRegression()
+    if isinstance(elem.model, NNet):
+        model = MLPRegressor(hidden_layer_sizes=elem.model.hidden_layers)
+    if isinstance(elem.model, SVM):
+        kernel = elem.model.kernel
+        if isinstance(kernel, Linear):
+            model = SVR(C=elem.model.cost, kernel='linear')
+        if isinstance(kernel, Polynomial):
+            model = SVR(C=elem.model.cost, kernel='poly', degree=kernel.degree)
+        if isinstance(kernel, Radial):
+            model = SVR(C=elem.model.cost, kernel='rbf', gamma=kernel.gamma)
+    model.fit(X_train, y_train)
+    return model.score(X_test, y_test)
 
 def fitness_function(data):
     def ff(ind: Start):
-        model = evaluate(ind, data)
-        if not model:
+        score = evaluate(ind, data)
+        if not score:
             return 999999999999999999
         else:
-            model.fit(
-                data[0], 
-                data[1],
-            )
-
-        return 1
+            return score
     return ff
 
-data = pd.read_csv('data/ChickWeight.csv')
-import IPython as ip
-ip.embed()
+vars = {
+    'MINIMIZE': False,
+    'NUMBER_OF_ITERATIONS': 108,
+    'MIN_INIT_DEPTH': None,
+    'MIN_DEPTH': None,
+    'MAX_INIT_DEPTH': None,
+    'MAX_DEPTH': 6,
+    'POPULATION_SIZE': 30,
+    'ELITSM': 5,
+    'TARGET_FITNESS': 1,
+}
 
 if __name__ == "__main__":
     representations = [ 'ge', 'dsge', 'treebased' ]
@@ -100,7 +145,7 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--representation", dest="representation", type=int, default=0)
     args = parser.parse_args()
 
-    run_experiments(grammar, ff=fitness_function(data), ff_test=fitness_function(data), folder_name="cnn", seed=args.seed, representation=representations[args.representation])
+    run_experiments(grammar, ff=fitness_function((X_train, y_train)), ff_test=fitness_function((X_test, y_test)), folder_name="hpo", seed=args.seed, vars=vars, representation=representations[args.representation])
 
 
 
---
+++ Gengy_implementation/parallel_comparison_hpo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=rep_comp_cnn         # Job name
+#SBATCH --nodes=1                       # Run all processes on a single node	
+#SBATCH --ntasks=1                      # Run a single task		
+#SBATCH --cpus-per-task=1               # Number of CPU cores per task
+#SBATCH --mem=10gb                      # Job memory request
+#SBATCH --time=48:00:00                 # Time limit hrs:min:sec
+#SBATCH --output=logs/parallel_%j.log   # Standard output and error log
+
+#SBATCH --array=0-89%5                  # iterate values between 0 and 59, inclusive
+
+# sbatch parallel_comparison.sh treebased
+cd ..
+pip install -r requirements.txt
+cd Gengy_implementation
+python examples/HPO.py -s $(expr $SLURM_ARRAY_TASK_ID % 30) -r $(expr $SLURM_ARRAY_TASK_ID / 30)
