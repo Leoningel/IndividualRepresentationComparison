9047acc3e46a8b527201904ead2bba7db689f8e2
Leon Ingelse <leoningelse@gmail.com>
2022-12-16 12:56:28 +0000

    Changed bash files for cnn as it needs more memory and time

--- src/parallel_comparison_cnn.sh
+++ src/parallel_comparison_cnn.sh
@@ -3,13 +3,13 @@
 #SBATCH --nodes=1                       # Run all processes on a single node	
 #SBATCH --ntasks=1                      # Run a single task		
 #SBATCH --cpus-per-task=1               # Number of CPU cores per task
-#SBATCH --mem=100mb                     # Job memory request
-#SBATCH --time=10:00:00                 # Time limit hrs:min:sec
+#SBATCH --mem=8gb                       # Job memory request
+#SBATCH --time=48:00:00                 # Time limit hrs:min:sec
 #SBATCH --output=logs/parallel_%j.log   # Standard output and error log
 
-#SBATCH --array=0-89                    # iterate values between 0 and 59, inclusive
+#SBATCH --array=0-89%5                  # iterate values between 0 and 59, inclusive
 
 cd ..
 pip install -r requirements.txt
 cd src
-python run_example.py -s $(expr $SLURM_ARRAY_TASK_ID % 30) -r $(expr $SLURM_ARRAY_TASK_ID / 30) -e $*
+python run_example.py -s $(expr $SLURM_ARRAY_TASK_ID % 30) -r $(expr $SLURM_ARRAY_TASK_ID / 30) -e CNN
