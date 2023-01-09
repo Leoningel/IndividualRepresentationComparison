228ffc56fdf596507d7669d9bd968b556fd7ded8
Alcides Fonseca <me@alcidesfonseca.com>
2023-01-09 14:51:49 +0000

    Fixed requirements

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-pyenv install 3.11.1
-pyenv local 3.11.1
+if [[ $OSTYPE == 'darwin'* ]]; then
+    PYTHONVERSION="3.10.9"
+else
+    PYTHONVERSION="3.11.0"
+fi
+
+pyenv install --skip-existing $PYTHONVERSION
+pyenv local $PYTHONVERSION
 python -m venv venv
 source venv/bin/activate
 python -m pip install -r requirements.txt
