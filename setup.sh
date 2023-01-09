8a2057f3af32a2a3a8c118392559a62c046b8adc
Alcides Fonseca <me@alcidesfonseca.com>
2023-01-09 14:51:49 +0000

    Fixed requirements

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+if [[ $OSTYPE == 'darwin'* ]]; then
+    PYTHONVERSION="3.10.9"
+else
+    PYTHONVERSION="3.11.0"
+fi
+
+pyenv install --skip-existing $PYTHONVERSION
+pyenv local $PYTHONVERSION
+
+python -m venv venv
+source venv/bin/activate
 python -m pip install -r requirements.txt
 mkdir Gengy_implementation/results
 # mkdir archive
