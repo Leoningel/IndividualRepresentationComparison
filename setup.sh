b13c8730daed545e53ac2b4b6e36a8078105d6ce
Alcides Fonseca <me@alcidesfonseca.com>
2023-01-16 15:32:34 +0000

    Updated Python version

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+
 if [[ $OSTYPE == 'darwin'* ]]; then
     PYTHONVERSION="3.10.9"
 else
-    PYTHONVERSION="3.11.0"
+    PYTHONVERSION="3.11.1"
 fi
 
 pyenv install --skip-existing $PYTHONVERSION
@@ -11,6 +20,3 @@ python -m venv venv
 source venv/bin/activate
 python -m pip install -r requirements.txt
 
-mkdir Gengy_implementation/results
-# mkdir archive
-# mkdir logs
--- update.sh
+++ update.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+
 git pull 
 pyenv local 3.11.1
 source venv/bin/activate
