15ff7a0efbe28d54c46701a05e7db02e27ef6f76
Alcides Fonseca <me@alcidesfonseca.com>
2023-03-23 10:07:28 +0000

    Solved conflict

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-if [[ $OSTYPE == 'darwin'* ]]; then
-    PYTHONVERSION="3.10.9"
-else
-    PYTHONVERSION="3.11.0"
-=======
 #!/usr/bin/env bash
 
 set -o errexit
@@ -19,15 +12,10 @@ if [[ $OSTYPE == 'darwin'* ]]; then
     PYTHONVERSION="3.10.9"
 else
     PYTHONVERSION="3.11.1"
->>>>>>> virtualenv
 fi
 
 pyenv install --skip-existing $PYTHONVERSION
 pyenv local $PYTHONVERSION
-<<<<<<< HEAD
-
-=======
->>>>>>> virtualenv
 python -m venv venv
 source venv/bin/activate
 python -m pip install -r requirements.txt
